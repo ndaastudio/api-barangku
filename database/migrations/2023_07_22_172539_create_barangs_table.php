9bc8033782c44386d8fb76ff6cec01ada3f4ddf6
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-07-26 20:40:13 +0700

    add schema database

---
+++ database/migrations/2023_07_22_172539_create_barangs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Akun;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Akun::class);
+            $table->string('nama_barang');
+            $table->string('kategori');
+            $table->string('status');
+            $table->string('extend_status');
+            $table->string('jumlah_barang');
+            $table->string('letak_barang');
+            $table->string('keterangan');
+            $table->dateTime('jadwal_rencana');
+            $table->dateTime('jadwal_notifikasi');
+            $table->integer('progress')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
---
+++ database/migrations/2023_07_22_185013_create_jasas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Akun;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jasa', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Akun::class);
+            $table->string('nama_jasa');
+            $table->string('kategori');
+            $table->string('jumlah_jasa');
+            $table->string('letak_jasa');
+            $table->string('keterangan');
+            $table->dateTime('jadwal_rencana');
+            $table->dateTime('jadwal_notifikasi');
+            $table->integer('progress')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jasa');
+    }
+};
