41c90160d2d999ce7fecd0b60f79c264389aa9c1
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-08-18 01:20:37 +0700

    created landing page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/syarat-dan-ketentuan', [HomeController::class, 'syaratDanKetentuan']);
