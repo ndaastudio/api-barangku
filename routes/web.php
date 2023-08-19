c65f0858ea453629663ffdb522145643d4494fc2
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-08-19 22:52:55 +0700

    created register tester

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Web\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/syarat-dan-ketentuan', [HomeController::class, 'syaratDanKetentuan']);
+Route::get('/register-tester', [HomeController::class, 'registerTester']);
+Route::post('/register-tester', [HomeController::class, 'submitRegisterTester']);
