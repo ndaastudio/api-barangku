7b841827efa70b5b58efff7117d8d2a09036637c
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-10-01 00:24:55 +0700

    refactor: :ambulance: form for "Atur Pembayaran"

---
+++ app/Http/Requests/FormAturPembayaran.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormAturPembayaran extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama_rekening' => 'required|string|min:3|max:255',
+            'nama_bank' => 'required|string|min:3|max:255',
+            'nomor_rekening' => 'required|string|min:6|max:255',
+            'harga_kode' => 'required|integer',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nama_rekening.required' => 'Nama pemilik rekening tidak boleh kosong',
+            'nama_rekening.string' => 'Nama pemilik rekening harus berupa huruf',
+            'nama_rekening.min' => 'Nama pemilik rekening minimal 3 karakter',
+            'nama_rekening.max' => 'Nama pemilik rekening maksimal 255 karakter',
+            'nama_bank.required' => 'Nama bank tidak boleh kosong',
+            'nama_bank.string' => 'Nama bank harus berupa huruf',
+            'nama_bank.min' => 'Nama bank minimal 3 karakter',
+            'nama_bank.max' => 'Nama bank maksimal 255 karakter',
+            'nomor_rekening.required' => 'Nomor rekening tidak boleh kosong',
+            'nomor_rekening.string' => 'Nomor rekening harus berupa huruf',
+            'nomor_rekening.min' => 'Nomor rekening minimal 6 karakter',
+            'nomor_rekening.max' => 'Nomor rekening maksimal 255 karakter',
+            'harga_kode.required' => 'Harga kode daftar tidak boleh kosong',
+            'harga_kode.integer' => 'Harga kode daftar harus berupa angka',
+        ];
+    }
+}
---
+++ resources/views/web/dashboard/mitra/Pengaturan/kuota-marketing/atur-pembayaran/index.blade.php
@@ -0,0 +1,66 @@
+@extends('web.templates.dashboard')
+
+@section('content')
+	<div class="card">
+		<form action="{{ route('kuota-marketing.atur-pembayaran') }}" method="POST">
+			@csrf
+			@method('PUT')
+			<h5 class="card-header">Silahkan atur pembayaran Anda</h5>
+			<div class="card-body">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="mb-3">
+							<label for="nama_rekening" class="form-label">Nama Pemilik Rekening</label>
+							<input name="nama_rekening" type="text"
+								class="form-control {{ $errors->has('nama_rekening') ? 'is-invalid' : '' }}" id="nama_rekening"
+								placeholder="Contoh: Budi" value="{{ old('nama_rekening', $mitra->nama_rekening) }}">
+							@if ($errors->has('nama_rekening'))
+								<div class="invalid-feedback">{{ $errors->first('nama_rekening') }}</div>
+							@endif
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="mb-3">
+							<label for="nama_bank" class="form-label">Nama Bank</label>
+							<input name="nama_bank" type="text" class="form-control {{ $errors->has('nama_bank') ? 'is-invalid' : '' }}"
+								id="nama_bank" placeholder="Contoh: BNI" value="{{ old('nama_bank', $mitra->nama_bank) }}">
+							@if ($errors->has('nama_bank'))
+								<div class="invalid-feedback">{{ $errors->first('nama_bank') }}</div>
+							@endif
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="mb-3">
+							<label for="nomor_rekening" class="form-label">Nomor Rekening</label>
+							<input name="nomor_rekening" type="number"
+								class="form-control {{ $errors->has('nomor_rekening') ? 'is-invalid' : '' }}" id="nomor_rekening"
+								placeholder="Contoh: 1234567890" value="{{ old('nomor_rekening', $mitra->nomor_rekening) }}">
+							@if ($errors->has('nomor_rekening'))
+								<div class="invalid-feedback">{{ $errors->first('nomor_rekening') }}</div>
+							@endif
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="mb-3">
+							<label for="harga_kode" class="form-label">Harga Kode Daftar</label>
+							<div class="input-group">
+								<span class="input-group-text" id="harga_kode">Rp</span>
+								<input name="harga_kode" type="number"
+									class="form-control {{ $errors->has('harga_kode') ? 'is-invalid' : '' }}" id="harga_kode"
+									placeholder="Harga yang akan dijual" value="{{ old('harga_kode', $mitra->harga_kode) }}">
+								@if ($errors->has('harga_kode'))
+									<div class="invalid-feedback">{{ $errors->first('harga_kode') }}</div>
+								@endif
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="card-footer d-flex">
+				<button type="submit" class="btn btn-secondary create-new btn-primary ms-auto">
+					Simpan
+				</button>
+			</div>
+		</form>
+	</div>
+@endsection
