6572811d533785c9cae89bb669760312d32ff080
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-10-01 00:30:48 +0700

    feat: add mitra column for table in admin panel

--- resources/views/web/dashboard/MemberMitra/kode-daftar/contents/admin/index.blade.php
+++ resources/views/web/dashboard/MemberMitra/kode-daftar/contents/admin/index.blade.php
@@ -4,6 +4,7 @@
 			<thead>
 				<tr class="text-center">
 					<th scope="col">No.</th>
+					<th scope="col">Mitra</th>
 					<th scope="col">Kode Daftar</th>
 					<th scope="col">Nomor Telepon</th>
 					<th scope="col">Paket</th>
@@ -16,6 +17,7 @@
 				@foreach ($members as $member)
 					<tr>
 						<th scope="row" class="text-center">{{ $no }}</th>
+						<td>{{ $member->mitra->nama }}</td>
 						<td>{{ $member->kode_daftar }}</td>
 						<td>{{ $member->nomor_telepon }}</td>
 						<td>{{ $member->paket == '0' ? 'Trial 7 Hari' : '1 Tahun (365 Hari)' }}</td>
