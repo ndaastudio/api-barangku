925eebb3c2cd03e61858cb4ff9a497d5735538b9
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-01-06 16:01:51 +0700

    feat: get storage link in model

--- app/Models/Mitra.php
+++ app/Models/Mitra.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Mitra extends Model
 {
@@ -15,5 +16,10 @@ class Mitra extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function previewCV()
+    {
+        Storage::url($this->dokumen_cv);
     }
 }
